abfd7953dc05811473eba6dd3b7d6fcea0ed3989
thmcheung <thmarcus.cheung@mail.utoronto.ca>
2024-06-08 17:44:26 -0400

    so far so good?

--- src/compiler.py
+++ src/compiler.py
@@ -1,2 +1,17 @@
-def bruh():
-    print(1)
+def run_meta_statement(statement):
+    match statement:
+        case ".exit":
+            return -1 #exit statement
+        case _:
+            return 2 #unrecognized meta command
+
+def run_sql(statement):
+    # needs to be tokenized and parsed
+    return 3
+
+
+def process(statement):
+    if statement[0] == '.':
+        return run_meta_statement(statement)
+    else:
+        return run_sql(statement)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
+# Feedback number
+# 0 : success
+# 1 : failed
+# 2 : unrecognized meta command
+# 3 : unrecognized sql command
+# -1 : success quit
 import compiler
 
 if __name__ == '__main__':
     while True:
         statement = input("db > ")
-        if statement[:5].lower() == ".exit":
+        feedback = compiler.process(statement)
+        if feedback == 0:
+            print("success")
+            #not implemented yet
+        elif feedback == 1:
+            print("failure")
+        elif feedback == 2:
+            print("unrecognized meta command")
+        elif feedback == 3:
+            print("unrecognized sql command")
+        elif feedback == -1:
+            print("exit successful")
             break
-        else:
-            print("unrecognized")
-            #add this to compiler
     exit()
