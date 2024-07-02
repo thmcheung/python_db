07574e95bee314ce7ab216ae5ec3967baeceaed0
thmcheung <thmarcus.cheung@mail.utoronto.ca>
2024-07-01 23:58:06 -0400

    hello

--- src/backend.py
+++ src/backend.py
@@ -10,47 +10,53 @@ import os
 
 global PAGE_SIZE, TABLE_MAX_PAGES, ROW_SIZE
 PAGE_SIZE = 4096
-TABLE_MAX_PAGES = 40
-TABLE_MAX_ROWS = 40
-ROW_SIZE = 40
+TABLE_MAX_PAGES = 100
+ROW_SIZE = 256
 
 
-class pager:
-    def __init__(self, file_descriptor: int, file_length: int, pages: list):
+class Pager:
+    def __init__(self, file_descriptor: int, file_length: int):
         self.file_descriptor = file_descriptor
         self.file_length = file_length
-        self.pages = pages
-    
-    def flush(self, page_num, size):
-        os.lseek(self.file_descriptor, page_num * PAGE_SIZE, os.SEEK_SET)
-        os.write(self.file_descriptor, self.pages[page_num][:size])
+        self.pages = [None] * TABLE_MAX_PAGES
 
-class table:
-    #this will be a b-tree later
+class Table:
     def __init__(self, num_rows, pager):
         self.num_rows = num_rows
-        self.pager = []
+        self.pager = pager
     
     def add(self, r):
         self.num_rows += 1
-        self.rows.append(r)
+        self.pager.append(r)
 
 #returns a table that contains a pager
 def db_open(filename):
     p = pager_open(filename)
     num_rows = p.file_length / ROW_SIZE
-    t = table(num_rows, p)
-    return table
+    t = Table(num_rows, p)
+    return Table
 
 def pager_open(filename):
-    os.open(filename, os.O_RDWR | os.O_CREAT, 0o600)
-    file_length = os.lseek(filename, 0, os.SEEK_END)
-    pages = [None] * TABLE_MAX_PAGES
-    p = pager(filename, file_length, pages)
+    with open(filename, 'a+b') as f:  #Open the file in append + binary mode
+        fd = f.fileno()  #Get the file descriptor
+        f.seek(0, os.SEEK_END)
+        file_length = f.tell()
+    p = Pager(fd, file_length)
+    return p
 
-def get_page(p:pager, page_num: int):
+def get_page(pager:Pager, page_num):
     if page_num > TABLE_MAX_PAGES:
         return 1
-    if p.pages[page_num] == None:
-        return 1
-    return p.pages[page_num]
+    if pager.pages[page_num] == None:
+        #none in memory, have to go in file
+        num_pages = pager.file_length / PAGE_SIZE
+        if pager.file_length % PAGE_SIZE != 0:
+            num_pages += 1
+    if page_num <= num_pages:
+        os.lseek(pager.file_descriptor, page_num * PAGE_SIZE, os.SEEK_SET)
+        bytes_read = os.read(pager.file_descriptor, PAGE_SIZE)
+        if bytes_read == -1:
+            return 1
+        else:
+            pager.pages[page_num] = bytes_read
+            return pager.pages[page_num]
